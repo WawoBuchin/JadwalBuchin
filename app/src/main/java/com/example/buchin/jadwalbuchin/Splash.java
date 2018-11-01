5f20a01a39a385d807a91f24be74ee9d9fc885f0
Lenovo <velliana@yahoo.com>
2018-11-01 17:38:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/buchin/jadwalbuchin/Splash.java
@@ -0,0 +1,23 @@
+package com.example.buchin.jadwalbuchin;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+    private static int Splash = 1200;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(Splash.this, MainActivity.class);
+                startActivity(splashIntent);
+            }
+        },Splash);
+    }
+}

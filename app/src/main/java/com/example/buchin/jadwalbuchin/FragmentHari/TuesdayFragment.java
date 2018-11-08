994ceff531cb64771bcddc2fb6f8d64319832ccd
wawoBuchin <ratihteni@yahoo.co.id>
2018-11-08 12:41:17 +0700

    haiii

--- app/src/main/java/com/example/buchin/jadwalbuchin/FragmentHari/TuesdayFragment.java
+++ app/src/main/java/com/example/buchin/jadwalbuchin/FragmentHari/TuesdayFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.buchin.jadwalbuchin.R;
+
 
 public class TuesdayFragment extends Fragment {
 
